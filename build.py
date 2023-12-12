e34f9fc513b5d1be4de40d5ccc0888d8f9a2620b
Koala <75808182+infamouskoala@users.noreply.github.com>
2023-12-12 17:10:50 +0530

    Delete build.py

--- build.py
+++
@@ -1,9 +0,0 @@
-'''
-When running the code on linux, my bot prefix was messing up.
-The file was becoming multiline and I didn't know why, so this is a temp fix for me.
-You may ignore this file and run the main.py only.
-'''
-
-file = open("prefix.txt","w")
-file.write(input("prefix: "))
-file.close()
